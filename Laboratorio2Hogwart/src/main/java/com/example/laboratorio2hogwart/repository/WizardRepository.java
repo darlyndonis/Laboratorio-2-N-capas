c4ed2f23153781f9cf14fa0cbfd02b7a6388f6ce
darlyndonis <00083823@uca.edu.sv>
2026-05-08 22:22:09 -0600

    Feat: create WizardRepository with custom queries

--- Laboratorio2Hogwart/src/main/java/com/example/laboratorio2hogwart/repository/WizardRepository.java
+++ Laboratorio2Hogwart/src/main/java/com/example/laboratorio2hogwart/repository/WizardRepository.java
@@ -1,5 +1,14 @@
 package com.example.laboratorio2hogwart.repository;
 
-public interface WizardRepository {
+import com.example.laboratorio2hogwart.domain.entity.Wizard;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface WizardRepository extends JpaRepository<Wizard, UUID> {
+    List<Wizard> findByIsDeathEater(Boolean isDeathEater);
+    List<Wizard> findByPatronus(String patronus);
 }
