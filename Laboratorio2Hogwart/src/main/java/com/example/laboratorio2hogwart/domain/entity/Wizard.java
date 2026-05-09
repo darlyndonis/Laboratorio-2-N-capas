5eb6eb2c9c30a621167ea9297dbbfd7df7b3fc0f
darlyndonis <00083823@uca.edu.sv>
2026-05-08 22:17:46 -0600

    Feat: create Wizard entity

--- Laboratorio2Hogwart/src/main/java/com/example/laboratorio2hogwart/domain/entity/Wizard.java
+++ Laboratorio2Hogwart/src/main/java/com/example/laboratorio2hogwart/domain/entity/Wizard.java
@@ -1,6 +1,35 @@
 package com.example.laboratorio2hogwart.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "Wizard")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Wizard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "house")
+    private String house;
+
+    @Column(name = "patronus")
+    private String patronus;
+
+    @Column(name = "is_death_eater")
+    private Boolean isDeathEater;
 }
+
