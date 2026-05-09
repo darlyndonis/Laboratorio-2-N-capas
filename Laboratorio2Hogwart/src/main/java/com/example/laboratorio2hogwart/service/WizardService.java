5002fdda0d182a1304095ec70d8a728d3b29e74f
darlyndonis <00083823@uca.edu.sv>
2026-05-08 22:24:57 -0600

    Feat: define WizardService interface

--- Laboratorio2Hogwart/src/main/java/com/example/laboratorio2hogwart/service/WizardService.java
+++ Laboratorio2Hogwart/src/main/java/com/example/laboratorio2hogwart/service/WizardService.java
@@ -1,5 +1,15 @@
 package com.example.laboratorio2hogwart.service;
 
-public interface WizardService {
+import com.example.laboratorio2hogwart.domain.entity.Wizard;
 
+import java.util.List;
+import java.util.UUID;
+
+public interface WizardService {
+    void createWizard(Wizard wizard);
+    List<Wizard> getAllWizards();
+    List<Wizard> getDeathEaters();
+    List<Wizard> getByPatronus(String patronus);
+    void updateWizard(UUID id, Wizard wizard);
+    void deleteWizard(UUID id);
 }
